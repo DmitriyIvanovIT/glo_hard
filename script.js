1a75bd497287ef614d1c0333044b9b5594497e20
DmitriyIvanovIT <strela199720@gmail.com>
2020-08-07 09:41:02 +0300

    Усложненые ДЗ

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// let num = 266219;
+let num = 266219;
 
-// let res = 1;
+let res = 1;
 
-// while(num) {
-//     res *= num % 10;
-//     num = Math.floor(num / 10);
-// } 
+while(num) {
+    res *= num % 10;
+    num = Math.floor(num / 10);
+} 
 
-// console.log('Результат: ', res);
-// console.log('Результат в третьей степени: ', res ** 3);
-// console.log(String(res).slice(0,2));
+console.log('Результат: ', res);
+console.log('Результат в третьей степени: ', res ** 3);
+console.log(String(res).slice(0,2));
