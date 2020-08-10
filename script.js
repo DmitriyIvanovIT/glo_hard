08c5c44bc58045e0338b6f9ac47ed1162516b8e2
DmitriyIvanovIT <strela199720@gmail.com>
2020-08-10 11:02:53 +0300

    feat: Complicated Homework # 3 | /script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// let num = 266219,
-// res = 1;
+let num = 266219,
+res = 1;
 
-// while(num) {
-//     res *= num % 10;
-//     num = Math.floor(num / 10);
-// } 
+while(num) {
+    res *= num % 10;
+    num = Math.floor(num / 10);
+} 
 
-// console.log('Результат: ', res);
-// res **= 3;
-// console.log('Результат в третьей степени: ', res);
-// console.log(String(res).slice(0,2));
+console.log('Результат: ', res);
+res **= 3;
+console.log('Результат в третьей степени: ', res);
+console.log(String(res).slice(0,2));
 
 // Задача 1
 let lang = prompt('Введите название языка (ru/en)');
@@ -18,9 +18,14 @@ const langArr = [];
 langArr['ru']  = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
 langArr['en'] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-if (lang === 'ru') console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье')
-else if (lang === 'en') console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday')
-else console.log('Что-то пошло не так');
+if (lang === 'ru') {
+    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+}
+else if (lang === 'en') {
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+}
+else {console.log('Что-то пошло не так');
+};
 
 switch (lang) {
     case 'ru':
