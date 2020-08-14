5509ed3820305cde7cd4aff4c665c5152e1f187e
DmitriyIvanovIT <strela199720@gmail.com>
2020-08-14 17:55:27 +0300

    feat: Working with arrays | /script.js

--- script.js
+++ script.js
@@ -1,96 +1,17 @@
 "use strict";
 // ДЗ 2
-let num = 266219,
-res = 1;
-
-while(num) {
-    res *= num % 10;
-    num = Math.floor(num / 10);
-} 
-
-console.log('Результат: ', res);
-res **= 3;
-console.log('Результат в третьей степени: ', res);
-console.log(String(res).slice(0,2));
-
-// ДЗ 3
-// Задача 1
-let lang = prompt('Введите название языка (ru/en)');
-
-const langArr = [];
-langArr['ru']  = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-langArr['en'] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-if (lang === 'ru') {
-    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-}
-else if (lang === 'en') {
-    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-}
-else {console.log('Что-то пошло не так');
-};
-
-switch (lang) {
-    case 'ru':
-        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-        break;
-    case 'en':
-        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-        break;
-    default: 
-        console.log('Что-то пошло не так');
-}
-
-console.log(langArr[lang]);
-
-// Задача 2
-let namePerson = prompt('Введите имя?');
-
-let result = namePerson === 'Артем' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент';
-
-console.log(result);
-
-// ДЗ 4
-let strInput = prompt('Введите строку');
-
-const stringOperation = str => {
-    if (typeof str !== 'string') {
-        return console.log('Введена не строка');
-    };
-
-    str = str.trim();
-    if (str.length > 30) {
-        str = str.substring(0, 30) + '...';
-    }
-    console.log(str);
-};
-
-stringOperation(strInput);
-
-// ДЗ 5
-for (let i = 2; i < 101; i++) {
-    let numberDivisors = 1;
-
-    for (let j = 2; j <= i; j++) {
-        if (i % j === 0) {
-            numberDivisors += 1;
-        }
-    };
-
-    if (numberDivisors === 2) {
-        console.log(`Простое число: ${i} делители этого числа: 1 и ${i}`);
-    }
-};
 
 // ДЗ 7
+const weekUl = document.querySelector('.week');
 let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 
 let date = new Date();
 
-console.log(week.join(', '));
-week.forEach(item => {
-    console.log(item);
+weekUl.textContent = '';
+week.forEach((item, i) => {
+    weekUl.insertAdjacentHTML('beforeend', `
+        <li class="day-week${(i === 5 || i === 6) ? ' weekend' : ''}${(i === date.getDay() - 1 )? ' today' : ''}">
+            ${item}
+        </li>
+    `)
 });
-
-console.log(`%c${week.slice(5).join(', ')}`, 'font-style: italic');
-console.log(`%c${week[date.getDay() - 1]}`, 'font-weight: bold');
