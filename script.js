c4889c0cc3b8df09e2861fc3826714f2c6b97a16
DmitriyIvanovIT <strela199720@gmail.com>
2020-08-18 10:30:00 +0300

    feat: date and time output | /index.html, /script.js, /style.css

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 "use strict";
-// ДЗ 7
-const weekUl = document.querySelector('.week');
-let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+// ДЗ 9
+const timeOne = document.querySelector('.time-one'),
+    timeTwo = document.querySelector('.time-two');
+
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    month = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
 
 let date = new Date();
+const hourTransformation = item => {
+        if (item === 1 || item === 21) {
+            return 'час';
+        } else if (item === 0 || (item > 4 && item < 21)) {
+            return 'часов';
+        } else {
+            return 'часа';
+        }
+    },
+    minuteTransformation = item => {
+        if (item % 10 === 1) {
+            return 'минута';
+        } else if (item % 10 > 1 && item % 10 < 5) {
+            return 'минуты';
+        } else {
+            return 'минут';
+        }
+    },
+    secondTransformation = item => {
+        if (item % 10 === 1) {
+            return 'секунда';
+        } else if (item % 10 > 1 && item % 10 < 5) {
+            return 'секунды';
+        } else {
+            return 'секунд';
+        }
+    },
+    addZero = item => item < 10 ? '0' + item : item,
+    timeRender = () => {
+        let today = (date.getDay() !== 0) ? (date.getDay() - 1) : 6;
+        timeOne.textContent = '';
+        timeTwo.textContent = '';
 
-let today = (date.getDay() !== 0) ? (date.getDay() - 1) : 6;
+        timeOne.textContent = `Сегодня ${week[today]}, ${date.getDate()} ${month[date.getMonth()]} ${date.getFullYear()} года, ${date.getHours()} ${hourTransformation(date.getHours())}  ${date.getMinutes()} ${minuteTransformation(date.getMinutes())} ${date.getSeconds()} ${secondTransformation(date.getSeconds())} `;
 
-weekUl.textContent = '';
-week.forEach((item, i) => {
-    weekUl.insertAdjacentHTML('beforeend', `
-        <li class="day-week${(i === 5 || i === 6) ? ' weekend' : ''}${(i === today)? ' today' : ''}">
-            ${item}
-        </li>
-    `)
-});
+        timeTwo.textContent = `${addZero(date.getDate())}.${addZero(date.getMonth())}.${date.getFullYear()}- ${addZero(date.getHours())}:${addZero(date.getMinutes())}:${addZero(date.getSeconds())}`;
+    };
+
+setInterval(timeRender, 1000);
