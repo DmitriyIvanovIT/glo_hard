8b6d6568171e68c32154feb0ca865d272d369c17
DmitriyIvanovIT <strela199720@gmail.com>
2020-08-26 11:31:20 +0300

    feat: Working with class inheritance | index.js

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 'use strict';
 
-const button = document.querySelector('button'), 
-h1 = document.querySelector('h1'),
-body = document.querySelector('body');
-
-const arr = ['a', 'b', 'c', 'd', 'e', 'f'];
-
-const randomInteger = function (min, max) {
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-},
-generationColor = () => {
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        let number = randomInteger(0, 15);
-        color +=  number > 9 ? arr[number - 10] : number;
-        color += '';
+class First {
+    hello() {
+        console.log('Привет я метод родителя!');
     }
+}
 
-    body.style.backgroundColor = color;
-    h1.textContent = color;
-    button.style.color = color;
-};
+class Second extends First {
+    hello() {
+        super.hello();
+        console.log('А я наследуемый метод!');
+    }
+}
 
-button.addEventListener('click', generationColor);
+const first = new First(),
+second = new Second();
 
+first.hello();
+second.hello();
