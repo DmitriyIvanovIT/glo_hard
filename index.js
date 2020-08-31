ef4b96ca36f032759c3a8cb627a1c85e3553bdc7
DmitriyIvanovIT <strela199720@gmail.com>
2020-08-31 12:37:47 +0300

    feat: Working with animations | index.js

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 'use strict';
 
-class First {
-    hello() {
-        console.log('Привет я метод родителя!');
+const cub = document.querySelector('.cub'),
+    play = document.querySelector('.play'),
+    reset = document.querySelector('.reset');
+
+let left = 0,
+    topLengt = 0;
+
+let driveCub;
+
+let animate = false;
+
+const driverCub = () => {
+    driveCub = requestAnimationFrame(driverCub);
+    if (left < 600 && topLengt === 0) {
+        left++;
+        cub.style.left = left + 'px';
+    } else if (left === 600 && topLengt < 300) {
+        topLengt++;
+        cub.style.top = topLengt + 'px';
+    } else if (left > 0 && topLengt > 0) {
+        left -= 2;
+        topLengt--;
+        cub.style.left = left + 'px';
+        cub.style.top = topLengt + 'px';
     }
-}
+};
 
-class Second extends First {
-    hello() {
-        super.hello();
-        console.log('А я наследуемый метод!');
+
+play.addEventListener('click', () => {
+    reset.disabled = false;
+    if (animate) {
+        cancelAnimationFrame(driveCub);
+        play.textContent = 'play';
+
+        animate = false;
+    } else {
+        animate = true;
+        play.textContent = 'pause';
+        driveCub = requestAnimationFrame(driverCub);
     }
-}
 
-const second = new Second();
+});
 
-second.hello();
+reset.addEventListener('click', () => {
+    cancelAnimationFrame(driveCub);
+    play.textContent = 'play';
+    left = 0;
+    topLengt = 0;
+    cub.style.left = left + 'px';
+    cub.style.top = topLengt + 'px';
+    reset.disabled = true;
+});
